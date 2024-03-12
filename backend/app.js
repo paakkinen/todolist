a6439aed001fb2715beccb8e1ad86ad347815a69
paakkinen <amin.paakkinen@edu.salpaus.fi>
2024-03-12 13:29:42 +0200

    moi

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,53 @@
+const createError = require("http-errors");
+const express = require("express");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const logger = require("morgan");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+const { Kitten } = require("./mongo/schema");
+ 
+app.use(express.json());
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
 mongoose.connect(
 
     "mongodb+srv://paakkinen:<password>@atlascluster.ft5ndi5.mongodb.net/"
-    
-    );
-    app.use(function (req, res, next) {
 
-        next(createError(404));
-        
-        });
-        
-        app.use(function (err, req, res, next) {
-        
-        res.locals.message = err.message;
-        
-        res.locals.error = req.app.get("env") === "development" ? err : {};
-        
-        res.status(err.status || 500);
-        
-        res.send("error");
-        
-        });
-        
-        const port = 3001;
-        
-        app.listen(port, () => {
-        
-        console.log(`Example app listening on port ${port}`);
-        
-        });
-        
-        module.exports = app
+);
+
+mongoose.connection
+    .once("open", function () {
+       console.log("Connected"); 
+    })
+    .on("error", function (error) {
+        console.log("CONNECTION ERROR:", error);
+    })
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+    });
+    
+    // error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+    
+    // render the error page
+    res.status(err.status || 500);
+    res.send("error");
+    });
+    
+    const port = 3001;
+    app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+    });
+    
+    module.exports = app
