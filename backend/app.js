a4b0140c82212d489dd8b6bdca65aefbfca66b7f
paakkinen <amin.paakkinen@edu.salpaus.fi>
2024-03-13 09:52:58 +0200

    moi

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const createError = require("http-errors");
 const express = require("express");
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const usersRouter = require("./routes/user");
 const logger = require("morgan");
 const cors = require("cors");
 const app = express();
@@ -13,11 +13,11 @@ app.use(cors());
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/user", usersRouter);
 
 mongoose.connect(
 
-    "mongodb+srv://paakkinen:<password>@atlascluster.ft5ndi5.mongodb.net/"
+    "mongodb+srv://paakkinen:S3Bhy7jGb9wW23FF@atlascluster.ft5ndi5.mongodb.net/"
 
 );
 
